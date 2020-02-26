17d9f2e6965fe56664baacd30e7cd5fdc7df1e35
Federico <f.piscitelli1@campus.unimib.it>
2020-02-26 19:59:08 +0100

    Aggiunta view login e template header

---
+++ Implementazione/view/login.view.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Gruppo Aereo 4</title>
+    <?php include("../template/header.php") ?>
+    <link type="text/css" rel="stylesheet" href="../css/style.css" />
+</head>
+
+<body>
+
+	<div id="booking" class="section">
+		<div class="section-center">
+			<div class="container">
+				<div class="row">
+				    <div class="col-md-3"></div>
+					<div class="booking-form col-md-6">
+						<form>
+                            <div class="container">
+                                <div class="row text-center mb-3">
+                                    <h2>Accedi alla tua area riservata</h2>
+                                </div>
+                            </div>
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<span class="form-label">E-mail:</span>
+										<input class="form-control" type="text" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<div class="form-group">
+										<span class="form-label">Password</span>
+										<input class="form-control" type="password" placeholder="Password">
+									</div>
+								</div>
+							</div>
+							<div class="row">
+							    <div class="col-md-3">
+							        </div>
+								<div class="col-md-6">
+									<div class="form-btn">
+										<button class="submit-btn">Login</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php include("../template/footer.php") ?>
+</body>
+
+</html>
