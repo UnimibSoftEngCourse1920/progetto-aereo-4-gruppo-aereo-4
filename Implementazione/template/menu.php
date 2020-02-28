ad128767d2a64bed1a7b1a6412375b69ff5f1bde
Omar Zaher <o.zaher@campus.unimib.it>
2020-02-28 17:40:13 +0100

    Modifiche sito

--- Implementazione/template/menu.php
+++ Implementazione/template/menu.php
@@ -16,5 +16,6 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <a href="/"><img id="logo" src="img/logo.png" /></a>
     </nav>
 </div>
