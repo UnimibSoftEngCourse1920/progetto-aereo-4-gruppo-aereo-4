edfa22a891f086baefe57819d99e0cc919d2a79a
Federico <f.piscitelli1@campus.unimib.it>
2020-03-01 12:08:47 +0100

    Aggiunta view per cambio prenotazione e fix acquisto

--- Implementazione/view/cambiaprenotazione.view.php
+++ Implementazione/view/cambiaprenotazione.view.php
@@ -2,47 +2,60 @@
 <html lang="it">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gruppo Aereo 4 - Acquisto</title>
+	<title>Gruppo Aereo 4 - Modifica Prenotazione</title>
     <?php include("../template/header.php") ?>
     <link type="text/css" rel="stylesheet" href="../css/style.css" />
 </head>
 
 <body>
-<?php include("../template/menu.php") ?>
-<div class="body-cont px-4">
-    <div class="row">
-        <div class="container">
-
-            <div class="login-form">
-                <h4 class="py-2">Completa il tuo acquisto</h4>
-                <span>Per ogni passeggero inserisci nome e cognome:</span>
-                <form >
-                    <div class="container p-0 mt-3">
-                        <div class="row">
-                        <?php
-                            for($i=0; $i<3; $i++){?>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <span class="form-label">Nome</span>
-                                        <input class="form-control" type="text" placeholder="Nome">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <span class="form-label">Cognome</span>
-                                        <input class="form-control" type="text" placeholder="Cognome">
-                                    </div>
-                                </div>
-                         <?php  } ?>
+    <?php include("../template/menu.php") ?>
+    <div class="container body-cont">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="login-form col-md-6">
+                    <div class="container">
+                        <div class="row text-center mb-3">
+                            <h2>Modifica la tua prenotazione</h2>
                         </div>
                     </div>
-                    <?php if(isset($_SESSION["fedelta"])) {?>
-
-                    <?php } else { ?>
+                    <div class="container">
+                        <div class="row mb-3">
+                            <div class="col-md-6 p-0">
+                                <b>#:</b>
+                            </div>
+                            <div class="col-md-6 ">
+                                <b>Numero persone:</b>
+                            </div>
+                            <div class="col-md-6 p-0">
+                                <b>Da:</b>
+                            </div>
+                            <div class="col-md-6 ">
+                                <b>A:</b>
+                            </div>
+                        </div>
+                    </div>
+                <form>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span class="form-label">Data</span>
+                                <input class="form-control" type="date" placeholder="Data">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <span class="form-label">Ora</span>
+                                <select class="form-control" placeholder="Ora">
+                                    <option> 9.30</option>
+                                    <option> 12.30</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
                         <h4 class="py-2">Inserisci i dati di pagamento</h4>
                         <div class="container p-0 mt-3">
                             <div class="row">
@@ -78,14 +91,14 @@
                                 </div>
                             </div>
                         </div>
-                    <?php } ?>
-
+                        </div>
+                    </div>
                     <div class="row">
                         <div class="col-md-3">
-                        </div>
+                            </div>
                         <div class="col-md-6">
                             <div class="form-btn">
-                                <button class="submit-btn">Completa acquisto</button>
+                                <button class="submit-btn">Modifica prenotazione</button>
                             </div>
                         </div>
                     </div>
@@ -93,9 +106,7 @@
             </div>
         </div>
     </div>
-</div>
-<script src="<?php echo $dir ?>js/script.js"></script>
-<?php include("../template/footer.php") ?>
+	<?php include("../template/footer.php") ?>
 </body>
 
 </html>
