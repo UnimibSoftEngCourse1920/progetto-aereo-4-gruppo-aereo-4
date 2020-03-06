f180a52732d7cf41ec3ae0f78b73af605cb49f73
Omar Zaher <o.zaher@campus.unimib.it>
2020-03-06 21:18:15 +0100

    Fix facade

--- Implementazione/app/models/servizi/DBFacade.php
+++ Implementazione/app/models/servizi/DBFacade.php
@@ -15,7 +15,7 @@ require_once("database/PostoDB.php");
 require_once("database/PrenotazioneDB.php");
 require_once("database/PromozioneDB.php");
 require_once("database/VoloDB.php");
-require_once("database/OIDDB.php");
+//require_once("database/OIDDB.php");
 
 
 class DBFacade{
@@ -39,7 +39,7 @@ class DBFacade{
         $this->gestori['Prenotazione'] = new PrenotazioneDB();
         $this->gestori['Promozione'] = new PromozioneDB();
         $this->gestori['Volo'] = new VoloDB();
-        $this->gestori['OID'] = new OIDDB();
+        //$this->gestori['OID'] = new OIDDB();
     }
 
     public static function getIstance(){
