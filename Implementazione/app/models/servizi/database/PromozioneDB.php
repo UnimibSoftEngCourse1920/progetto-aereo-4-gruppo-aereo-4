d8104ab20d9973587e15d4b68456facce4aa7848
Federico <f.piscitelli1@campus.unimib.it>
2020-03-10 21:15:41 +0100

    Rimozione var_dump

--- Implementazione/app/models/servizi/database/PromozioneDB.php
+++ Implementazione/app/models/servizi/database/PromozioneDB.php
@@ -10,9 +10,7 @@ class PromozioneDB extends AbstractDB{
     //Da rivedere fabio
     //percentuale
     protected function generatePutQuery($obj){
-        var_dump($obj->isFedelta());
         $var = sprintf("INSERT INTO Promozione VALUES ('%s', '%s', '%s', '%s', %u, %u)", $obj->getOID(), $obj->getDataInizio(), $obj->getDataFine(), $obj->getNome(), (int)$obj->isFedelta(), (int)$obj->getSconto());
-        var_dump($var);
         return $var;
     }
 }
