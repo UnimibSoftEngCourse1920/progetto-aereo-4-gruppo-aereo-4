63e70fd3dbeb280e31b30693683e18aedb1ae184
Federico <f.piscitelli1@campus.unimib.it>
2020-03-10 21:14:39 +0100

    Inserimento e cancellazione promozione funzionanti

--- Implementazione/app/models/servizi/database/PromozioneDB.php
+++ Implementazione/app/models/servizi/database/PromozioneDB.php
@@ -10,7 +10,9 @@ class PromozioneDB extends AbstractDB{
     //Da rivedere fabio
     //percentuale
     protected function generatePutQuery($obj){
-        $var = sprintf("INSERT INTO Promozione VALUES ('%s', '%s', '%s', '%s', '%s', '%s')", $obj->getOID(), $obj->getDataInizio(), $obj->getDataFine(), $obj->getNome(), $obj->isFedelta(), $obj->getSconto());
+        var_dump($obj->isFedelta());
+        $var = sprintf("INSERT INTO Promozione VALUES ('%s', '%s', '%s', '%s', %u, %u)", $obj->getOID(), $obj->getDataInizio(), $obj->getDataFine(), $obj->getNome(), (int)$obj->isFedelta(), (int)$obj->getSconto());
+        var_dump($var);
         return $var;
     }
 }
