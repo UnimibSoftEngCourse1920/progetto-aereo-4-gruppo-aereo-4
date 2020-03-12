147cf619d495fa81c9b6a54d8a65f2ca2995af33
Davide Rendina <d.rendina2@campus.unimib.it>
2020-03-12 22:36:11 +0100

    Fix salvataggio pagamento-carta

--- Implementazione/app/models/servizi/database/PagamentoConCartaDB.php
+++ Implementazione/app/models/servizi/database/PagamentoConCartaDB.php
@@ -8,7 +8,7 @@ class PagamentoConCartaDB
 {
     public function generatePutQuery($obj){
         //TODO get istituto è un oggetto o un codice??
-        $query = "INSERT INTO PagamentoConPunti VALUES ('%s',%d,'%s','%s')";
+        $query = "INSERT INTO PagamentoConCarta VALUES ('%s',%d,'%s','%s')";
         return sprintf($query, $obj->getOID(), $obj->getImporto(), $obj->getData(), $obj->getIstituto()->getOID());
     }
 
