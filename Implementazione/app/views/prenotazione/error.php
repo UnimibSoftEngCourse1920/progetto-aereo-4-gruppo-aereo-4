84fd56aa2421262801fabee7c7b0319117da225d
Federico <f.piscitelli1@campus.unimib.it>
2020-03-14 16:41:18 +0100

    Aggiunta file errore prenotazione

---
+++ Implementazione/app/views/prenotazione/error.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Gruppo Aereo 4</title>
+    <?php include("../app/template/header.php") ?>
+</head>
+<body>
+<?php include("../app/template/menu.php") ?>
+<div class="container pb-5 pt-5 mt-5">
+    <div class="row">
+        <div class="col text-center">
+            <h2><?=$data["error"]?></h2>
+            <em class="fas fa-time-circle mt-5" style="font-size: 150px; color: #7091BE;"></em>
+        </div>
+    </div>
+</div>
+<?php include("../app/template/footer.php") ?>
+</body>
+</html>
