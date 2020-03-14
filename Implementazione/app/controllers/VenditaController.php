3e01605ea9a2a5025af20d11b5237e96101bcf18
Omar Zaher <o.zaher@campus.unimib.it>
2020-03-14 21:36:35 +0100

    Risoluzione code smell

--- Implementazione/app/controllers/VenditaController.php
+++ Implementazione/app/controllers/VenditaController.php
@@ -39,7 +39,6 @@ class VenditaController extends Controller {
                                                 "tariffa" => $tariffa]);
 	}
 	
-	//TODO: DB
 	public function cambiaData($idPrenotazione, $idCliente, $idNuovoVolo, $nuovaTariffa, $metodoPagamento = "", $carta = "") {
         $error = "";
         $nuovoVolo = $this->registroVoli->getVolo($idNuovoVolo);
@@ -65,7 +64,7 @@ class VenditaController extends Controller {
                         $error = "Non è stato possibile effettuare il cambio data.";
                     }
                 } else {
-                    $errore = "La prenotazione non è ancora stata acquistata.";
+                    $error = "La prenotazione non è ancora stata acquistata.";
                 }
             } else {
                 $error = "La prenotazione appartiene ad un altro cliente.";
